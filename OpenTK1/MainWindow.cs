f1ed6ef19c0a2c27162a39f6e888972f149638c0
Abhishek Chakraborty <abhishek.forall@gmail.com>
2018-05-25 22:25:44 +0530

    Initialize Window
    Following tutorial at https://www.codeproject.com/Articles/1167212/OpenGL-with-OpenTK-in-Csharp-Part-Initialize-the-G

--- OpenTK1/MainWindow.cs
+++ OpenTK1/MainWindow.cs
@@ -11,5 +11,55 @@ namespace OpenTK1
 {
     public class MainWindow: GameWindow
     {
+        public MainWindow(): base(720, 720, OpenTK.Graphics.GraphicsMode.Default, 
+                                  "dreamstatecoding", GameWindowFlags.Default, 
+                                  DisplayDevice.Default, 4, 0, 
+                                  OpenTK.Graphics.GraphicsContextFlags.ForwardCompatible )
+        {
+            Title += ": OpenGL Version: " + GL.GetString(StringName.Version);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            CursorVisible = true;
+        }
+
+        //protected override void OnUpdateFrame(FrameEventArgs e)
+        //{
+        //    HandleKeyboard();
+        //}
+
+        //private void HandleKeyboard()
+        //{
+        //    var keyState = Keyboard.GetState();
+
+        //    if (keyState.IsKeyDown(Key.Escape))
+        //    {
+        //        Exit();
+        //    }
+        //}
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            Title = $"(Vsync: {VSync}) FPS: {1f / e.Time:0}";
+
+            OpenTK.Graphics.Color4 backColor;
+
+            backColor.A = 1.0f;
+            backColor.R = 0.1f;
+            backColor.G = 0.1f;
+            backColor.B = 0.3f;
+            GL.ClearColor(backColor);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            SwapBuffers();
+        }
     }
 }
